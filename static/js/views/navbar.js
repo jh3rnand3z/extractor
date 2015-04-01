77ad0687e8b94986a74b522c53bb34f714b21fc4
Jean Chassoul <chassoul@gmail.com>
2015-04-01 13:26:19 -0600

    init work on mexican demo

--- static/js/views/navbar.js
+++ static/js/views/navbar.js
@@ -3,7 +3,8 @@ fun.views.navbar = Backbone.View.extend({
 	events: {
         "click #details-report-btn": 'detailsReport',
         "click #fun-signup": 'signupPopup',
-        "click #fun-login": 'loginPopup'
+        "click #fun-login": 'loginPopup',
+        "click #login-btn": 'login'
 	},
 
     initialize: function(options) {
@@ -77,12 +78,66 @@ fun.views.navbar = Backbone.View.extend({
         console.log('navbar detail reports')
     },
 
-    loginPopup: function(){
+    loginPopup: function(event){
+        event.preventDefault();
         console.log("login");
+        // Cache the DOM stuff
+        this.loginError = this.$('#signin-alert');
+        
+        // form inputs
+        this.username = this.$('#username');
+        this.password = this.$('#password');
     },
 
-    signupPopup: function(){
+    signupPopup: function(event){
+        event.preventDefault();
         console.log("signup");
+    },
 
+    login: function(event){
+        event.preventDefault();
+        
+        var loginError = this.loginError;
+        var username = this.username.val();
+        var password = this.password.val();
+        var view = this;
+
+        var loginSuccess = function(view, loginError){
+            // Clear the stuff from the inputs ;)
+            view.$('#username').val('');
+            view.$('#password').val('');
+            loginError.removeClass("show" ).addClass("hide");
+            fun.utils.redirect(fun.conf.hash.dashboard);
+        };
+        
+        fun.utils.login(username, password, {
+            success : function(jqXHR, textStatus){
+                // currently this success call is never executed
+                // the success stuff is going on case 200 of the error function.
+                // Why? well... I really don't fucking know...
+                loginSuccess(view, loginError);
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                switch(jqXHR.status) {
+                    case 403:
+                        var message = fun.utils.translate("usernameOrPasswordError");
+                        loginError.find('p').html(message);
+                        loginError.removeClass("hide" ).addClass("show");
+                        break;
+                    case 200:
+                        // Check browser support
+                        if (typeof(Storage) != "undefined") {
+                            // Store
+                            localStorage.setItem("username", username);
+                        }
+                        loginSuccess(view, loginError);
+                        break;
+                    default:
+                        console.log('the monkey is down');
+                        break;
+                }
+            }
+        
+        });
     }
 });
