33a41ac1fc91a1c5683ce76add8345a3b6e79a0f
Jean Chassoul <chassoul@gmail.com>
2015-04-06 12:07:45 -0600

    clxCbacks and stuff

--- static/js/views/signup.js
+++ static/js/views/signup.js
@@ -62,6 +62,7 @@ fun.views.signup = Backbone.View.extend({
             rules,
             validationRules,
             callbacks,
+            clxCbacks,
             validForm;
         event.preventDefault();
         signupError = this.signupError;
@@ -151,6 +152,15 @@ fun.views.signup = Backbone.View.extend({
         if (validForm){
             //event.preventDefault();
 
+            clxCbacks = {
+                success: function(){
+                    console.log('CLX Success');
+                },
+                error: function(model, error){
+                    console.log('CLX Error');
+                }
+            };
+
             this.clxRegister = new fun.models.Register();
             this.clxRegister.save({
                 "Culture" : "en-US",
@@ -163,7 +173,7 @@ fun.views.signup = Backbone.View.extend({
                     "CountryCode": "1",
                     "CellPhone": phone
                 }
-            });
+            }, clxCbacks);
 
             this.model = new fun.models.Account();
             this.model.save(
