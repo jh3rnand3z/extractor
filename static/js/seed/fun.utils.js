b691b1e476d6e921db61aee0c09feae414d0e296
Jean Chassoul <chassoul@gmail.com>
2015-05-11 13:23:59 -0600

    check if we can read params from js

--- static/js/seed/fun.utils.js
+++ static/js/seed/fun.utils.js
@@ -213,6 +213,17 @@ fun.utils.openInIframe = function(iframeId, url){
 };
 
 
+/*
+*Get Parameter By Name
+*/
+fun.utils.getParameterByName = function(name){
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+
 /**
  * Hide all the UI stuff
  */
