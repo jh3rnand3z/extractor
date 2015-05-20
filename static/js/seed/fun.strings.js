47b2814d68d5ee3e7be8c38618a4a4e9fc8b403d
Jean Chassoul <chassoul@gmail.com>
2015-05-20 14:37:59 -0600

    ok some some

--- static/js/seed/fun.strings.js
+++ static/js/seed/fun.strings.js
@@ -12,7 +12,7 @@ fun.strings = {
     manager: "Manager",
     admin: "Admin",
     nameOnCard: "Name on Card",
-    theLast4CCDigits: "The last 4 Card digits",
+    theLast4CCDigits: "Last 4 Digits",
     clientInformation: "Client Information",
     cardType: "Card Type",
     iofun: "PreWay Pay",
