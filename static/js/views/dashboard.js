3ccd033b472e4eafdcea698c73c7b3a94fbb4721
Jean Chassoul <chassoul@gmail.com>
2015-05-20 15:45:42 -0600

    some dashboards rules other's not

--- static/js/views/dashboard.js
+++ static/js/views/dashboard.js
@@ -305,5 +305,11 @@ fun.views.dashboard = Backbone.View.extend({
         event.preventDefault();
         var view = this;
         console.log('log new client and popup the stuff');
+
+        $('#clientModal').modal({
+            'show': true,
+            //'backdrop': 'static',
+            //'keyboard': true
+        });
     }
 });
