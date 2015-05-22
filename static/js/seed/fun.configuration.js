393123a5f1f3cae7a6b2548175c3685702376edf
Jean Chassoul <chassoul@gmail.com>
2015-05-22 06:41:23 -0600

    go dev

--- static/js/seed/fun.configuration.js
+++ static/js/seed/fun.configuration.js
@@ -5,7 +5,7 @@
 var environment, url, productionURL, developmentURL;
 var tokken, tokkenRUN, tokkenDEV;
 
-environment = 'production';
+environment = 'development';
 
 productionURL = 'http://201.149.49.175:9027';
 developmentURL = 'http://201.149.49.181:9027';
