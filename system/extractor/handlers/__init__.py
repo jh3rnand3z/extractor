70a3cbc3c6a810555aa75018af2fad4f9281f81c
Jean Chassoul <chassoul@gmail.com>
2015-06-08 18:22:40 -0600

    log stuff

--- system/extractor/handlers/__init__.py
+++ system/extractor/handlers/__init__.py
@@ -118,6 +118,8 @@ class BaseHandler(web.RequestHandler):
 
         elif error is not None:
             logging.warning(str_error)
+
+            logging.error(struct, scheme, error, reason)
             
             message = {
                 'error': u'nonsense',
