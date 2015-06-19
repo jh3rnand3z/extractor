e5946ae180d7acd74bea334288f23c94cfb16df3
Jean Chassoul <chassoul@gmail.com>
2015-06-19 17:51:39 -0600

    fix the model and stuff

--- system/extractor/messages/companies.py
+++ system/extractor/messages/companies.py
@@ -24,53 +24,29 @@ class Company(models.Model):
         Company Object Data Structure
     '''
     uuid = types.UUIDType(default=uuid.uuid4)
-    uniqueid = types.StringType()
+    state_province = types.StringType()
+    incorporated_state_province = types.StringType()
+    fax = types.StringType()
+    vat_tax_id_file_number = types.StringType()
+    incorporated_number = types.StringType()
+    zip_postal = types.StringType()
+    dba = types.StringType()
+    federal_tax_id = types.StringType()
+    incoportated_country = types.StringType()
+    telephone = types.StringType()
+    city_town = types.StringType()
+    country_company = types.StringType()
+    subsidiary_reg_num = types.StringType()
+    company_name = types.StringType()
+    company_email = types.StringType()
+    account_type = types.StringType()
+    password = types.StringType()
+    subsidiary_name = types.StringType()
+    email = types.StringType()
+    street_address = types.StringType()
+    incorporated_address = types.StringType()
 
-    clid = types.StringType()
-    callerid = types.StringType()
-
-    account = types.StringType()
-    accountcode = types.StringType()
-    userfield = types.StringType()
-    
-    assigned = types.BooleanType(default=False)
-    checked = types.BooleanType(default=False)
-
-    public = types.BooleanType(default=False)
-
-    source = types.StringType()
-    destination = types.StringType()
-    
-    channel = types.StringType()
-    source_channel = types.StringType()
-    
-    context = types.StringType()
-    dcontext = types.StringType()
-    destination_context = types.StringType()
-
-    destination_number = types.StringType()
-    
-    dstchannel = types.StringType()
-    destination_channel = types.StringType()
-    
-    start = types.DateTimeType()
-    answer = types.DateTimeType()
-    end = types.DateTimeType()
-
-    duration = types.IntType()
-    billsec = types.IntType()
-    billing = types.IntType()
-
-    disposition = types.StringType()
     status = types.StringType()
-
-    amaflags = types.StringType()
-
-    lastapp = types.StringType()
-    lastdata = types.StringType()
-
-    recorded = types.BooleanType(default=False)
-    record_uri = types.StringType()
 
     checked = types.BooleanType(default=False)
     checked_by = types.StringType()
